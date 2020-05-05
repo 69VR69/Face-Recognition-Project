d7fdc96cb8dd1f1656b5d73dcae0f5a74b2889a5
69VR69 <44296242+69VR69@users.noreply.github.com>
2020-05-05 18:24:32 +0200

    fix frmAnnotate

--- StorageManager.vb
+++ StorageManager.vb
@@ -1,8 +1,9 @@
-﻿Imports System.IO
+﻿Imports System.Data.OleDb
+Imports System.IO
 
 Public Class StorageManager
-    Private fileName As String
-    Private filePath As String
+    Private fileName As String = "Annotation.csv"
+    Private filePath As String = "C:\Users\afouq\Google Drive\Projet tutoré\IHM Face Recognition Project\Code\FRP\Face-Recognition-Project\res\Annotation.csv"
     Private Sub openImage()
         Dim newFile As Stream = Nothing
         Dim fileDialog As New OpenFileDialog()
@@ -33,18 +34,64 @@ Public Class StorageManager
         End If
     End Sub
 
-    Private Sub createCsv()
+    Public Sub readAnnotation()
 
-    End Sub
+        'Create and config the tool to explore the csv file
+        Dim TextFileReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(filePath)
+        TextFileReader.TextFieldType = FileIO.FieldType.Delimited
+        TextFileReader.SetDelimiters(";")
 
-    Private Sub readAnnotation()
+        'Create variables to contain annotations while the program is running
+        Dim TextFileTable As DataTable = Nothing
+        Dim Column As DataColumn
+        Dim Row As DataRow
+        Dim ColumnOnRow As Int32
+        Dim ColumnCount As Int32
+        Dim CurrentRow As String()
 
+        While Not TextFileReader.EndOfData
+            Try
+                CurrentRow = TextFileReader.ReadFields() 'read row
+
+                If CurrentRow IsNot Nothing Then
+
+                    If TextFileTable Is Nothing Then 'check if the row we're going to fill into DataTable is empty
+
+                        TextFileTable = New DataTable()
+                        ColumnOnRow = CurrentRow.GetUpperBound(0)
+
+                        For ColumnCount = 1 To ColumnOnRow
+                            Column = New DataColumn()
+                            Column.DataType = System.Type.GetType("System.String")
+                            Column.ColumnName = "Column" & ColumnCount
+                            Column.Caption = "Col" & ColumnCount
+                            Column.ReadOnly = True
+                            Column.Unique = False
+                            TextFileTable.Columns.Add(Column)
+                        Next
+
+                    End If
+                    Row = TextFileTable.NewRow
+                    For ColumnCount = 1 To ColumnOnRow
+                        Row("Column" & ColumnCount) = CurrentRow(ColumnCount).ToString
+                    Next
+                    TextFileTable.Rows.Add(Row)
+
+                End If
+
+            Catch ex As _
+            Microsoft.VisualBasic.FileIO.MalformedLineException
+                MsgBox("Line " & ex.Message &
+                "is not valid and will be skipped.")
+            End Try
+        End While
+        TextFileReader.Dispose()
+        'Show data
+        My.Forms.TEST.DataGridView1.DataSource = TextFileTable
     End Sub
 
     Private Sub writeAnnotation()
 
     End Sub
-
-
 
 End Class
---
+++ TEST.vb
@@ -0,0 +1,6 @@
+﻿Public Class TEST
+    Private Sub TEST_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim StorageManager As New StorageManager
+        StorageManager.readAnnotation()
+    End Sub
+End Class
